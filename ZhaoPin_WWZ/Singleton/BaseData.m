8fe523b9d3159c537f7698cea4e488408ec623de
WateringRocky <piaowan.demand@qq.com>
2013-02-19 15:58:40 +0800

    Add  BaseData

---
+++ ZhaoPin_WWZ/Singleton/BaseData.m
@@ -0,0 +1,13 @@
+//
+//  BaseData.m
+//  ZhaoPin_WWZ
+//
+//  Created by Ibokan on 13-2-19.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import "BaseData.h"
+
+@implementation BaseData
+
+@end

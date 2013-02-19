4ffe84ae8b0c4d99c1449e91b56d5200304f531c
WateringRocky <piaowan.demand@qq.com>
2013-02-19 09:49:52 +0800

    Initial Commit

---
+++ ZhaoPin_WWZ/ZPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZPAppDelegate.h
+//  ZhaoPin_WWZ
+//
+//  Created by Ibokan on 13-2-19.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZhaoPin_WWZ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZhaoPin_WWZ
+//
+//  Created by Ibokan on 13-2-19.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZPAppDelegate class]));
+    }
+}

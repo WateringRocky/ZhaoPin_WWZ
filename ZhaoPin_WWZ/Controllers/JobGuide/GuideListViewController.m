106d24fdbb4c7d2fc03028e315757146da6b5f17
DavidWayne <329448575@qq.com>
2013-02-19 17:14:41 +0800

    change4

--- ZhaoPin_WWZ/Controllers/JobGuide/GuideListViewController.m
+++ ZhaoPin_WWZ/Controllers/JobGuide/GuideListViewController.m
@@ -25,15 +25,47 @@
 -(void)loadView
 {
     self.view = [[[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds]autorelease];
-    self.view.backgroundColor = [UIColor yellowColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+
 }
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.tableView = [[[UITableView alloc] initWithFrame:CGRectMake(0, 0, 320, 460) style:UITableViewStyleGrouped]autorelease];
-     
+    [self.view addSubview:self.tableView];
 }
-
+#pragma mark -
+#pragma mark -tableView Delegate-
+-(NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
+{
+    
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //push
+}
+#pragma mark -
+#pragma mark -Memory Managament- 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
